2c7bdba751ab1166bf35bb803bf2466caf074f8a
Сергій Ірза <f2.25irza.s@nltu.lviv.ua>
2025-10-07 11:49:25 +0300

    Виконана лабораторна робота №5

--- lab5/src/Main.java
+++ lab5/src/Main.java
@@ -1,13 +1,115 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+// Інтерфейс з поведінкою
+interface IVolumetric {
+    double getVolume();
+    boolean isSameVolume(IVolumetric other);
+}
+
+// Абстрактний клас, що реалізує інтерфейс
+abstract class Solid implements IVolumetric {
+    @Override
+    public boolean isSameVolume(IVolumetric other) {
+        return this.getVolume() == other.getVolume();
+    }
+}
+
+// Клас Cube
+class Cube extends Solid {
+    private final double a;            // довжина ребра
+    public static int count = 0;       // статичне поле
+
+    public Cube(double a) {
+        this.a = a;
+        count++;
+    }
+
+    @Override
+    public double getVolume() {
+        return Math.pow(a, 3);
+    }
+
+    public double getA() {
+        return a;
+    }
+}
+
+// Клас прямокутного паралелепіпеда
+class RectSolid extends Solid {
+    private final double c;    // довжина
+    private final double d;    // ширина
+    protected double h;        // висота
+
+    public RectSolid(double c, double d, double h) {
+        this.c = c;
+        this.d = d;
+        this.h = h;
+    }
+
+    @Override
+    public double getVolume() {
+        return c * d * h;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getD() {
+        return d;
+    }
+
+    public double getH() {
+        return h;
+    }
+}
+
+// Основний клас
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 1. Створення куба (тип класу)
+        System.out.print("Введіть довжину ребра ПЕРШОГО куба: ");
+        double a1 = scanner.nextDouble();
+        Cube cube1 = new Cube(a1);  // тип класу
+
+        // 2. Створення прямокутного паралелепіпеда (тип батьківського класу)
+        System.out.print("Введіть довжину прямокутного паралелепіпеда (C): ");
+        double c = scanner.nextDouble();
+        System.out.print("Введіть ширину прямокутного паралелепіпеда (D): ");
+        double d = scanner.nextDouble();
+        System.out.print("Введіть висоту прямокутного паралелепіпеда (H): ");
+        double h = scanner.nextDouble();
+        Solid rectSolid = new RectSolid(c, d, h);  // тип батьківського класу
+
+        // 3. Створення другого куба (тип інтерфейсу)
+        System.out.print("Введіть довжину ребра ДРУГОГО куба (через інтерфейс): ");
+        double a2 = scanner.nextDouble();
+        IVolumetric cube2 = new Cube(a2);  // тип інтерфейсу
+
+        System.out.println("\n=== Результати ===");
+        System.out.println("Об'єм ПЕРШОГО куба (тип класу): " + cube1.getVolume());
+        System.out.println("Об'єм прямокутного паралелепіпеда (тип батьківський клас): " + rectSolid.getVolume());
+        System.out.println("Об'єм ДРУГОГО куба (тип інтерфейс): " + cube2.getVolume());
+
+        // Порівняння об'єктів
+        System.out.println();
+        if (cube1.isSameVolume(rectSolid)) {
+            System.out.println("Об'єм ПЕРШОГО куба і прямокутного паралелепіпеда однаковий.");
+        } else {
+            System.out.println("Об'єм ПЕРШОГО куба і прямокутного паралелепіпеда різний.");
+        }
+
+        if (cube1.isSameVolume(cube2)) {
+            System.out.println("Об'єм ПЕРШОГО і ДРУГОГО кубів однаковий.");
+        } else {
+            System.out.println("Об'єм ПЕРШОГО і ДРУГОГО кубів різний.");
+        }
+
+        // Статичне поле
+        System.out.println("\nКількість створених кубів: " + Cube.count);
+
+        scanner.close();
     }
 }
